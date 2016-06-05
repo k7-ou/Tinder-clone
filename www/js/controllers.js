412b8896d0b97720614a81b4b40e75ee39cef217
Kohei Kitamoto <k7_ou@kitamotokouhei-no-MacBook-Air.local>
2016-06-05 16:00:37 +0900

    First Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,16 @@
+angular.module('thunder.controllers', ['ionic', 'thunder.services'])
+
+  /*
+  Controller for the discover page
+  */
+  .controller('DiscoverCtrl', function($scope) {})
+
+  /*
+  Controller for the favorites page
+  */
+  .controller('FavoritesCtrl', function($scope) {})
+
+  /*
+  Controller for our tabs
+  */
+  .controller('TabsCtrl', function($scope) {})
